6d69aaa37baec876ecb8ef803fb84f4da8371a64
yuval.d <3dyuval@gmail.com>
2025-08-13 12:09:26 +0300

    Added `large-files-scatch-buffer`

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,20 +7,28 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			social: [
+				{
+					icon: 'github',
+					label: 'GitHub',
+					href: 'https://github.com/withastro/starlight'
+				}
+			],
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+						{
+							label: 'Large Files/Scratch buffer',
+							slug: 'large-files-scatch-buffer'
+						}
+					]
 				},
 				{
 					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+					autogenerate: { directory: 'reference' }
+				}
+			]
+		})
+	]
 });
